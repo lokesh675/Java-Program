0f4174b614cb04a6c4434b7492a3ade8dde2e1cf
raja <lokeshraja64598@gmail.com>
2025-07-04 21:07:42 +0530

    sample

---
+++ src/resoures/ODDorEVEN.java
@@ -0,0 +1,15 @@
+package resoures;
+
+public class ODDorEVEN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=20;
+		if(num%2==0) {
+			System.out.print(true);
+		} else {
+				System.out.print(false);
+			}
+		}
+	}
+
